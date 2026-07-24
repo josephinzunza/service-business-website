2efcb32b9ef381abc487d97673cb4f9fdb8c0b80
Joseph Inzunza <josephhinzunza@gmail.com>
2026-07-24 14:59:25 -0700

    Improved navbar (more responsive)

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// // Code to toggle the visibility of 'Get a Quote' button
+
+// const navbarToggler = document.getElementById('navbarToggler');
+
+// navbarToggler.addEventListener('show.bs.collapse', event => {
+//     const getAQuoteBtn = document.getElementById('getAQuoteBtn');
+//     getAQuoteBtn.style.display = 'none';
+// });
+
+// navbarToggler.addEventListener('hide.bs.collapse', event => {
+//     const getAQuoteBtn = document.getElementById('getAQuoteBtn');
+//     getAQuoteBtn.style.display = 'visible';
+// });
